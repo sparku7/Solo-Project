056f2c0a51ab8000732281faf184a400e1c30301
sparku7 <sparku7@sky.com>
2024-08-01 10:38:04 +0100

    Added spring app

---
+++ Employee-Task-Manager-BackEnd/src/test/java/com/lbg/Employee/Task/Manager/BackEnd/MainTests.java
@@ -0,0 +1,13 @@
+package com.lbg.Employee.Task.Manager.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
