2412b86ea7a586e2c511569d4d73b91957380793
sparku7 <sparku7@sky.com>
2024-08-01 10:49:33 +0100

    added entities

---
+++ Employee-Task-Manager-BackEnd/src/main/java/Entities/Employee.java
@@ -0,0 +1,58 @@
+package Entities;
+
+import jakarta.persistence.*;
+import org.springframework.scheduling.config.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String position;
+
+    @OneToMany(mappedBy = "assignedEmployee", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+
+    public Employee(Long id, String name, String position, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ Employee-Task-Manager-BackEnd/src/main/java/Entities/Task.java
@@ -0,0 +1,56 @@
+package Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_employee_id")
+    private Employee assignedEmployee;
+
+    public Task(Long id, String name, String description, Employee assignedEmployee) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.assignedEmployee = assignedEmployee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Employee getAssignedEmployee() {
+        return assignedEmployee;
+    }
+
+    public void setAssignedEmployee(Employee assignedEmployee) {
+        this.assignedEmployee = assignedEmployee;
+    }
+}
+
